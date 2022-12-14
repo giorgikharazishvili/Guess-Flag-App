13cadc390d23c714fe0f8439debaf3a8ed7ceb75
giorgikharazishvili <giorgi.kharazishvili@hotmail.com>
2022-12-14 17:33:13 +0400

    Initial Commit

---
+++ GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by GIORGI's MacPro on 14.12.22.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
